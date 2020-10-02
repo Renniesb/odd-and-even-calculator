c11a2b2bdd59ab72223ba80d1a277dc4af3c2910
Rennie.Bevineau@AWS.CTUONLINE.EDU <Rennie.Bevineau@AWS.CTUONLINE.EDU>
2020-10-01 23:59:49 -0400

    odd and even calculator

---
+++ odd and even calculator.py
@@ -0,0 +1,13 @@
+#Rennie Bevineau
+
+def main():
+    
+    number=int(input("number?")) #must store user input to later test if value is odd or even
+
+    if(number%2==0): # if user input is divisible by 2 it is even
+        print("The number you entered is even")
+    else: # when user input is not divisible by 2 it is odd
+        print("The number you entered is odd") 
+
+
+main()
